975fb3924541f54c71cc5d4cf3742d3be4fe9365
Miguel Sbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2023-02-14 19:28:15 -0300

    feat: :sparkles: Resolve problema 1171

--- 1171.py
+++ 1171.py
@@ -1,6 +1,17 @@
 qtd = int(input())
 numbers = []
+sep_number = []
+
 
 for i in range(qtd):
     numbers.append(int(input()))
 
+i = 0
+while i < qtd:
+    if sep_number.__contains__(numbers[i]) == False:
+        sep_number.append(numbers[i])
+    i+= 1
+
+sep_number.sort()
+for i in sep_number:
+    print(str(i) + ' aparece ' + str(numbers.count(i)) + ' vez(es)')
